16a4960c97a819c7f6251e0acff08f211cf6a927
ShowMeTheCodePLZ <9333706+luowei0@user.noreply.gitee.com>
2022-09-25 19:36:32 +0800

    chore: 划分目录结构

---
+++ src/pages/table/table.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="content">
+    <Timetable class-name="timetable" />
+  </view>
+</template>
+
+<script setup lang="ts">
+import Timetable from "@components/Timetable.vue";
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.timetable {
+  height: 70vh;
+}
+</style>
