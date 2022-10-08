005062640570515ddec60aaa95a429dbf05b9c74
LuoWei <86441933@qq.com>
2022-10-08 21:01:03 +0800

    refactor: 重构首页布局和UI

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,15 +1,9 @@
+import { network } from "@/lib/network";
+
 export function login(username: string, password: string) {
-  return new Promise((resolve, reject) => {
-    uni.request({
-      url: "http://127.0.0.1:4523/mock/1672514/user/login",
-      method: "POST",
-      data: { username, password },
-      success: value => {
-        resolve(value.data);
-      },
-      fail: err => {
-        reject(err);
-      },
-    });
+  return network<BaseResponse<null>>({
+    url: "http://127.0.0.1:4523/mock/1672514/user/login",
+    method: "POST",
+    data: { username, password },
   });
 }
