3b9b381ad5e55eb7a1259b15381cf15a7aefeae1
LuoWei <86441933@qq.com>
2022-10-10 10:17:53 +0800

    feature: 完善lint逻辑

---
+++ src/enums/pages.ts
@@ -0,0 +1,3 @@
+export const INDEX = "/pages/table/table";
+export const TABLE = "/pages/table/table";
+export const PERSONAL = "/pages/personal/personal";
--- src/pages/table/table.vue
+++ src/pages/table/table.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import Timetable from "@components/Timetable";
+import Timetable from "@/components/TimeTable.vue";
 </script>
 
 <style scoped>
