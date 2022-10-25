640ebe1e414f2b16750758ba22d4ca1e8ba2151b
刘灿 <liucan.233@bytedance.com>
2022-10-25 11:33:47 +0800

    feat: 实现简易版网络拦截器

--- src/pages/table/table.vue
+++ src/pages/table/table.vue
@@ -5,7 +5,34 @@
 </template>
 
 <script setup lang="ts">
+import { Network } from "@/lib/network";
 import Timetable from "@components/Timetable.vue";
+
+const network = new Network({
+});
+
+network.onReq=(c)=>{
+  console.log(c)
+  return c
+}
+
+network.onRes=u=>{
+  console.log(u)
+  if(u.statusCode<200||u.statusCode>399){
+    throw new Error('2333')
+  }
+  return u.data
+}
+
+network.onErr=u=>{
+  console.log(u)
+}
+
+network.get<{ a: 1 }>("/swust/loginCas").then(
+  res => {
+    console.log(res);
+  },
+);
 </script>
 
 <style scoped>
