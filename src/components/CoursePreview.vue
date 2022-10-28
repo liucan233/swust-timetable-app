ef71c4a7fdbd6f9e82a67cbc37b3f1637a4e179a
刘灿 <liucan.233@bytedance.com>
2022-10-29 01:52:09 +0800

    feat: 新增周课程情况预览

---
+++ src/components/CoursePreview.vue
@@ -0,0 +1,17 @@
+<template>
+    <view class="container">
+
+    </view>
+</template>
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.container{
+    display: inline-block;
+    border: 1px solid black;
+    width: 80px;
+    height: 80px;
+    flex: 0 0 auto;
+}
+</style>
