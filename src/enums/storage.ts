8f18dfd11ffc3ff6b04b564cd4f3b2a418d6f77e
罗巍 <86441933@qq.com>
2022-10-30 14:15:37 +0800

    简化缓存逻辑

--- src/enums/storage.ts
+++ src/enums/storage.ts
@@ -1,11 +1,4 @@
 export enum Cookie {
-  /**登陆时cookie */
-  LOGIN_COOKIE = "LOGIN_COOKIE",
   /**一站式服务大厅cookie */
   CAS_COOKIE = "CAS_COOKIE",
-}
-
-export enum TimetableData {
-  COMMON_TIMETABLE = "COMMON_TIMETABLE",
-  LAB_TIMETABLE = "LAB_TIMETABLE",
 }
--- src/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-interface IBaseResponse<T> {
-  code: number;
-  msg: string;
-  data: T;
-}
