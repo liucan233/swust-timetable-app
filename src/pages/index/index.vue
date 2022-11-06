491409be2f05db3f6f81bcdd495ef3bdbd637c0f
刘灿 <liucan.233@bytedance.com>
2022-11-06 20:34:22 +0800

    refactor: 优化启动时跳转逻辑

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,10 +4,26 @@
 
 <script setup lang="ts">
 import { onBeforeMount } from "vue";
-import { TABLE } from "@enums/pages";
+import { TABLE, LOGIN } from "@enums/pages";
+import { credentials, valueIsFalsy } from "@utils/storage";
+import { showErrModal, showUnknownErrModal } from "@src/utils/common";
 
 onBeforeMount(() => {
-  uni.switchTab({ url: TABLE });
+  credentials
+    .getCasCookie()
+    .then(() => {
+      uni.redirectTo({ url: TABLE });
+    })
+    .catch(err => {
+      uni.redirectTo({ url: LOGIN });
+      if (!valueIsFalsy(err)) {
+        if (err instanceof Error) {
+          showErrModal("App启动时遇到问题", err.message);
+        } else {
+          showUnknownErrModal();
+        }
+      }
+    });
 });
 </script>
 
